deef9159ad2868488d64af8cbc03bbc75742b499
vahvake <62074587+Vahvelainen@users.noreply.github.com>
2022-05-06 17:14:24 +0300

    Delete visualization

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,9 @@
 import Frontpage from './views/Frontpage.js';
-import Visualization from './views/visualization.js';
-//import './aMapper.js';
 
 const router = async () => {
   const routes = [
+    //Only one route for now
     { path: '/', view: Frontpage },
-    { path: '/vs', view: Visualization }
   ];
   
   // Test each route for potential match
