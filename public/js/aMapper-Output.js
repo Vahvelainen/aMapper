5ca3feea3a7e6eb4d38166f0307f5f3fd55da0b8
vahvake <62074587+Vahvelainen@users.noreply.github.com>
2022-05-22 00:03:42 +0300

    Change default K to 10 and flip cluster order

--- public/js/aMapper-Output.js
+++ public/js/aMapper-Output.js
@@ -145,7 +145,7 @@ function findIndexFromArrays (matrix, vector) {
 
 function sortByLength(arr) {
   arr.sort(function(a, b) { 
-    return a.length - b.length;
+    return b.length - a.length;
   });
   return arr;
 } 
